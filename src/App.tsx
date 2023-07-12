1821c27c557a56d7658e2ad74d62ab5d104a35f8
ElisaBorba <elisaborbaa@gmail.com>
2023-07-12 16:46:09 -0300

    req 3 metade

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+
+
   return (
-    <div>
+  <div>
+    <header>
       <Title />
-      <Form />
-    </div>
+      <button onClick={ () => setShowForm(true) }>Cadastrar nova senha</button>
+      </header>
+      { showForm && <Form /> }
+      </div>
   );
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,37 +1,37 @@
 type SubmitProps = {
-    handleSubmit: () => void
+  handleSubmit: () => void
 };
 
 function Form(props: SubmitProps) {
-  const { handleSubmit } = props;
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        handleSubmit();
-  };
+const { handleSubmit } = props;
+const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      handleSubmit();
+};
 
-  return (
-    <form onSubmit={ onSubmit }>
+return (
+  <form onSubmit={ onSubmit }>
 
-      <label htmlFor="Nome do serviço">Nome do serviço:
-        <input type="text" id="Nome do serviço" />
-      </label>
-      
-      <label htmlFor="Login">Login:
-        <input type="text" id="Login" />
-      </label>
+    <label htmlFor="Nome do serviço">Nome do serviço:
+      <input type="text" id="Nome do serviço" />
+    </label>
+    
+    <label htmlFor="Login">Login:
+      <input type="text" id="Login" />
+    </label>
 
-      <label htmlFor="input-password">Senha:
-      <input type="password" id="input-password" />
-      </label>
+    <label htmlFor="input-password">Senha:
+    <input type="password" id="input-password" />
+    </label>
 
-      <label htmlFor="URL">URL:
-        <input type="text" id="URL" />
-      </label>
+    <label htmlFor="URL">URL:
+      <input type="text" id="URL" />
+    </label>
 
-      <button type="submit">Cadastrar</button>
-      <button>Cancelar</button>
-    </form>
-  );
+    <button type="submit">Cadastrar</button>
+    <button>Cancelar</button>
+  </form>
+);
 }
 
 export default Form;
