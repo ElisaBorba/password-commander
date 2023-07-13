b4b4e4e2f48ff541ba94b250c52aca5403b80dd1
ElisaBorba <elisaborbaa@gmail.com>
2023-07-13 00:34:21 -0300

    req4 metade

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,26 @@ import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
 
+export type FormValuesType = {
+  serviceName?: string,
+  login?: string,
+  password?: string,
+  url?: string,
+};
+
+const initialFormValues = {
+  serviceName: '',
+  login: '',
+  password: '',
+  url: '',
+};
+
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [showBtn, setShowBtn] = useState(true);
+  const [formValues, setFormValues] = useState<FormValuesType>(initialFormValues);
 
-  const handleClick = () => {
+    const handleClick = () => {
     setShowForm(true);
     setShowBtn(false);
   };
@@ -17,6 +32,21 @@ function App() {
     setShowForm(false);
   };
 
+  function validateForm(): boolean {
+    const { serviceName, login, password } = formValues;
+    const regexPassword = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,16}$/;
+    
+    if (!serviceName) {
+      return false
+    }
+    if (!login) {
+      return false
+    }
+    if (!regexPassword.test(password)) {
+      return false;
+    }
+  }
+
   return (
     <div>
       <header>
@@ -25,7 +55,7 @@ function App() {
           <button onClick={ handleClick }>Cadastrar nova senha</button>
         )}
       </header>
-      { showForm && <Form hideForm={ handleHideForm } /> }
+      { showForm && <Form hideForm={ handleHideForm } setFormValues={ setFormValues } formValues={ formValues } validateForm={ validateForm } /> }
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import { FormValuesType } from '../App';
 
 type FormProps = {
+  formValues: FormValuesType,
+  setFormValues: (prop: FormValuesType) => void,
   hideForm: () => void,
   handleSubmit: () => void,
+  validateForm: () => boolean,
 };
 
 function Form(props: FormProps) {
-  const { handleSubmit, hideForm } = props;
+  const { handleSubmit, hideForm, validateForm, formValues, setFormValues } = props;
+  const { serviceName, login, password, url } = formValues;
+
+console.log(formValues)
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     handleSubmit();
@@ -16,23 +23,34 @@ function Form(props: FormProps) {
     hideForm();
   };
 
+  const onChange  = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+    const { name, value } = target;
+    setFormValues({
+      ...formValues,
+      [name]: value
+
+    });
+    console.log(name)
+  }
+
   return (
     <form onSubmit={ onSubmit }>
       <label htmlFor="Nome do serviço">
         Nome do serviço:
-        <input type="text" id="Nome do serviço" />
+        <input type="text" name="serviceName" value={ serviceName } onChange={ onChange } required />
       </label>
       <label htmlFor="Login">
         Login:
-        <input type="text" id="Login" />
+        <input type="text" name="login" value={ login } onChange={ onChange } required />
       </label>
       <label htmlFor="input-password">
         Senha:
-        <input type="password" id="input-password" />
+        <input type="password" name="password" value={ password } onChange={ onChange }required />
       </label>
       <label htmlFor="URL">
         URL:
-        <input type="text" id="URL" />
+        <input type="text" name="url" value={ url } onChange={ onChange } />
       </label>
       <button type="submit">
         Cadastrar
@@ -43,3 +61,5 @@ function Form(props: FormProps) {
 }
 
 export default Form;
+
+// disabled={!validateForm()
